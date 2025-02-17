4f6791307397e489746fe04895aefdb5b6b21ade
NoahCrown <noahimperium@gmail.com>
2025-02-17 20:04:31 +0800

    Addded host back

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def handle_connect():
 if __name__ == '__main__':
     socketio.run(
         app,
+        host='0.0.0.0',
         debug=True,
         allow_unsafe_werkzeug=True,
     )  # For development only
